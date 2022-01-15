ca15d9509ec7e61f56d52775dbe3eb86e7367279
Greg Daybreak <greg00brown@gmail.com>
2022-01-14 16:14:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from a Github pull request!!!!!!"
+    return "This was deployed via GitHub!"
 
 
 if __name__ == "__main__":
